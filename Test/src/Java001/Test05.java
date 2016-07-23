70686ef137fc32c2dd913fcc7ce792b783bb33f7
Ryo Yaginuma <ryo.yaginuma@eiwa.com>
2016-07-23 15:46:34 +0900

    Test05作成

---
+++ Test/src/Java001/Test05.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2016 EIS Co., Ltd. All rights reserved.
+ */
+
+package Java001;
+
+/**
+ * @author 柳沼　諒 <br />
+ *         （クラス論理名） <br />
+ *         （説明） <br />
+ *         更新履歴 yyyy/mm/dd （更新者）：（説明） <br />
+ */
+public class Test05 {
+
+  /**
+   * （メソッド論理名） <br />
+   * （説明） <br />
+   * 
+   * @param args
+   */
+  public static void main( String[] args ) {
+    System.out.println( args[0] );
+    System.out.println( args[1] );
+    System.out.println( args[2] );
+
+  }
+
+}
