d635f5929c3f0a1a5df6d6c767631294b871c99e
Ryo Yaginuma <ryo.yaginuma@eiwa.com>
2016-07-23 16:28:29 +0900

    Test08,Test09を追加

---
+++ Test/src/Java002/Test09.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2016 EIS Co., Ltd. All rights reserved.
+ */
+package Java002;
+
+/**
+ * @author （作成者） <br />
+ *  （クラス論理名） <br />
+ *  （説明） <br />
+ *  更新履歴 yyyy/mm/dd （更新者）：（説明） <br />
+ */
+public class Test09 {
+
+  /**
+   * （メソッド論理名） <br />
+   * （説明） <br />
+   * @param args
+   */
+  public static void main( String[] args ) {
+    int ans=0;
+    int input = Integer.parseInt (args[0]);    
+    for(int i = 1; i <= input ; i++){
+      ans=ans+i;
+    }
+    System.out.println(ans);
+  }
+
+}
